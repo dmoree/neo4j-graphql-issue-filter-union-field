3b31a6b329e6915e46d98faea0e7389f90b0c75e
dmoree <dmoree@users.noreply.github.com>
2021-09-17 10:19:46 -0700

    reproduce: issue

--- src/graphql/typeDefs.ts
+++ src/graphql/typeDefs.ts
@@ -1,3 +1,29 @@
-import * as nodes from '@issue/graphql/nodes'
+import gql from 'graphql-tag'
 
-export const typeDefs = Object.values(nodes).map((node) => node.typeDefs)
+export const typeDefs = gql`
+  type Director {
+    id: ID! @id
+    name: String!
+
+    movies: [Movie!]! @relationship(type: "DIRECTED", direction: OUT)
+  }
+
+  type Actor {
+    id: ID! @id
+    name: String!
+
+    movies: [Movie!]! @relationship(type: "ACTED_IN", direction: OUT)
+  }
+
+  union Person = Director | Actor
+
+  type Movie {
+    id: ID! @id
+    title: String!
+
+    directors: [Director!]! @relationship(type: "DIRECTED", direction: IN)
+    actors: [Actor!]! @relationship(type: "ACTED_IN", direction: IN)
+
+    people: [Person!]! @relationship(type: "HAS_PERSON", direction: OUT)
+  }
+`
--- src/seed-database.ts
+++ src/seed-database.ts
@@ -1,6 +1,3 @@
-import faker from 'faker'
-import type { User } from '@issue/graphql/types'
-import { hashPassword } from '@issue/utils/password'
 import {
   clearDatabase,
   connect,
@@ -10,62 +7,52 @@ import {
 import { ogm } from '@issue/neo4j/graphql'
 
 export const seed = async () => {
-  const [User, Blog, Post, Comment] = ['User', 'Blog', 'Post', 'Comment'].map(
-    (name) => ogm.model(name)
+  const [Movie, Director, Actor] = ['Movie', 'Director', 'Actor'].map((name) =>
+    ogm.model(name)
   )
 
-  const defaultEmail = 'admin@admin.com'
-  const defaultPassword = 'password'
-
-  const { users } = await User.create<{ users: User[] }>({
-    input: await Promise.all(
-      [
-        [defaultEmail, defaultPassword],
-        [faker.internet.email(), faker.internet.password()],
-        [faker.internet.email(), faker.internet.password()],
-      ].map(async ([email, password]) => {
-        return {
-          email,
-          password: await hashPassword(password),
-        }
-      })
-    ),
-  })
-
-  await Blog.create({
-    input: users.map((user) => {
-      return {
-        name: faker.lorem.word(),
-        creator: {
-          connect: { where: { node: { id: user.id } } },
-        },
-        posts: {
-          create: new Array(1).fill(null).map(() => ({
-            node: {
-              title: faker.lorem.word(),
-              content: faker.lorem.paragraphs(4),
-              author: {
-                connect: { where: { node: { id: user.id } } },
-              },
-              comments: {
-                create: new Array(1).fill(null).map(() => {
-                  const u = users[Math.floor(Math.random() * users.length)]
-
-                  return {
-                    node: {
-                      content: faker.lorem.paragraph(),
-                      author: {
-                        connect: { where: { node: { id: u.id } } },
-                      },
-                    },
-                  }
-                }),
+  const {
+    movies: [theMatrix],
+  } = await Movie.create({
+    input: {
+      title: 'The Matrix',
+      people: {
+        Director: {
+          create: [
+            {
+              node: {
+                name: 'Lana Wachowski',
               },
             },
-          })),
+            {
+              node: {
+                name: 'Lilly Wachowski',
+              },
+            },
+          ],
         },
-      }
-    }),
+        Actor: {
+          create: [
+            {
+              node: {
+                name: 'Keanu Reeves',
+              },
+            },
+            {
+              node: {
+                name: 'Carrie-Ann Moss',
+              },
+            },
+          ],
+        },
+      },
+    },
+  })
+  await Director.update({
+    connect: { movies: { where: { node: { id: theMatrix.id } } } },
+  })
+  await Actor.update({
+    connect: { movies: { where: { node: { id: theMatrix.id } } } },
   })
 }
 
